402cde8925501a51ef61859327048f8df26c1e5f
abhaypanditzx <abhaypanditzx@gmail.com>
2022-10-09 18:07:11 +0530

    pehla project

---
+++ main.js
@@ -0,0 +1,9 @@
+function show(){
+      
+    let hide = document.getElementById('hide');
+     document.getElementById('para');
+if(para.style.display != 'none')
+{para.style.display = 'none'}
+
+else{para.style.display = 'block'}
+} 
